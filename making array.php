70b4d55cf8d65a71944bc4ef26e952f742c7578a
strxms <boyko.d.a@yandex.ru>
2021-08-10 12:23:51 +0300

    add (isset function): making isset function to random numbers (#2)

--- making array.php
+++ making array.php
@@ -1,10 +1,18 @@
 <?php
 
-const MIN_ARRAY=0;
-const MAX_ARRAY=100000;
+$start=microtime(true);
+define (MIN_ARRAY, 1);
+define (MAX_ARRAY, 1000000);
 
-for ($i=0; $i<MAX_ARRAY; $i++)
-{
-    $a[$i]=rand (0, 100000);
-}
-print_r($a);
+$numbers = array();
+do {
+    $already_here = rand(MIN_ARRAY,MAX_ARRAY);
+    if (!isset($numbers[$already_here]))
+    {
+        $numbers[$already_here] = true;
+    }
+} while (count($numbers) < MAX_ARRAY);
+print_r(array_keys($numbers));
+
+
+echo 'Time: '.round(microtime(true) - $start, 4).' сек.';
